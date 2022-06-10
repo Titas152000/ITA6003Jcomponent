166d25efff8dd0e8ccb7d2b9e591a0b4369a98ef
Titas152000 <81681019+Titas152000@users.noreply.github.com>
2022-06-10 19:22:36 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,5 @@
+
+    <div class="footer">
+       <p>&copy; 2022  Saloon Management System Admin Panel.</p>
+    </div>
+    
---
+++ admin/js/site.js
@@ -0,0 +1,69 @@
+// call this from the developer console and you can control both instances
+var calendars = {};
+
+$(document).ready( function() {
+
+
+  var thisMonth = moment().format('YYYY-MM');
+
+  var eventArray = [
+    { startDate: thisMonth + '-10', endDate: thisMonth + '-14', title: 'Multi-Day Event' },
+    { startDate: thisMonth + '-23', endDate: thisMonth + '-26', title: 'Another Multi-Day Event' }
+  ];
+
+
+
+  calendars.clndr1 = $('.cal1').clndr({
+    events: eventArray,
+ 
+    clickEvents: {
+      click: function(target) {
+        console.log(target);
+        if($(target.element).hasClass('inactive')) {
+          console.log('not a valid datepicker date.');
+        } else {
+          console.log('VALID datepicker date.');
+        }
+      },
+      nextMonth: function() {
+        console.log('next month.');
+      },
+      previousMonth: function() {
+        console.log('previous month.');
+      },
+      onMonthChange: function() {
+        console.log('month changed.');
+      },
+      nextYear: function() {
+        console.log('next year.');
+      },
+      previousYear: function() {
+        console.log('previous year.');
+      },
+      onYearChange: function() {
+        console.log('year changed.');
+      }
+    },
+    multiDayEvents: {
+      startDate: 'startDate',
+      endDate: 'endDate'
+    },
+    showAdjacentMonths: true,
+    adjacentDaysChangeMonth: false
+  });
+
+
+  $(document).keydown( function(e) {
+    if(e.keyCode == 37) {
+      // left arrow
+      calendars.clndr1.back();
+      calendars.clndr2.back();
+    }
+    if(e.keyCode == 39) {
+      // right arrow
+      calendars.clndr1.forward();
+      calendars.clndr2.forward();
+    }
+  });
+
+});
